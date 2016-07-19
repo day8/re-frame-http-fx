f9d016b32c7faa61bb29347157731ebb117f0da7
hipitihop <anonymous.amphibian@gmail.com>
2016-07-19 15:59:24 +1000

    test - add cljs.test/do-report :fail from ::bad-http-result

--- test/re_frame_http_fx/core_test.cljs
+++ test/re_frame_http_fx/core_test.cljs
@@ -13,22 +13,22 @@
 ;; ---- TESTS ------------------------------------------------------------------
 
 
-(deftest ajax-get
-  ;; Setup handler which returns a :http effect specifying an ajax-request.
+(deftest xhrio-get
+  ;; Setup effects handler with :http-xhrio specifying an ajax-request.
   ;; Note we specify optional :response-format to make sure our json result
   ;; has keywords for keys, and :timeout see the ajax-request API
   ;; for more details https://github.com/JulianBirch/cljs-ajax#ajax-request
-  ;; We specify an :on-failure for completenes but we don't expect the request
+  ;; We specify an :on-failure for completeness but we don't expect the request
   ;; to fail unless there is something wrong with your internet or github.
   (re-frame/def-event-fx
     ::http-test
     (fn [_context _event-v]
-      {:http {:method          :get
-              :uri             "https://api.github.com/orgs/day8"
-              :timeout         5000
-              :response-format (ajax/json-response-format {:keywords? true})
-              :on-success      [::good-http-result "test-token1"]
-              :on-failure      [::bad-http-result "test-token1"]}}))
+      {:http-xhrio {:method          :get
+                    :uri             "https://api.github.com/orgs/day8"
+                    :timeout         5000
+                    :response-format (ajax/json-response-format {:keywords? true})
+                    :on-success      [::good-http-result "test-token1"]
+                    :on-failure      [::bad-http-result "test-token1"]}}))
     (async done
 
       ;; setup success handler
@@ -46,10 +46,15 @@
       ;; setup failure handler
       (re-frame/def-event
         ::bad-http-result
-        (fn [db [_ _token error]]
-          (re-frame/console :error "Unexpected HTTP error, something wrong with your internet? " error)
+        (fn [db [_ token error]]
+          (is (= "test-token1" token) "expected: token passed through")
+          (cljs.test/do-report
+            {:type     :fail
+             :message  "Unexpected HTTP error, something wrong with your internet?"
+             :expected ::good-http-result
+             :actual   error})
           (done)
           db))
 
-      ;; kick things off
+      ;; kick off main handler
       (re-frame/dispatch [::http-test])))
