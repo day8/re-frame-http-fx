bbfa7b395e8282d4163b11ff6276485e6b8153bb
hipitihop <anonymous.amphibian@gmail.com>
2016-07-19 14:27:57 +1000

    test - don't use async setTimeout and instead call (done) direct from handlers). Add failure handler.

--- test/re_frame_http_fx/core_test.cljs
+++ test/re_frame_http_fx/core_test.cljs
@@ -2,7 +2,6 @@
   (:require
     [ajax.core     :as ajax]
     [cljs.test     :refer-macros [is deftest async]]
-    [clojure.set   :as set]
     [cljs.spec     :as s]
     [re-frame.core :as re-frame]
     [re-frame-http-fx.core]))
@@ -14,38 +13,43 @@
 ;; ---- TESTS ------------------------------------------------------------------
 
 
-(deftest ^:async test-get
-  (let [seen-events     (atom #{})
-        forwarder       (re-frame/add-post-event-callback
-                          (fn [source_event _]
-                            (swap! seen-events #(->> source_event first (conj %)))))]
-    ;; setup effects handler
-    (re-frame/def-event-fx
-      ::http-test
-      (fn [_context _event-v]
-        {:http {:method          :get
-                :uri             "https://api.github.com/orgs/day8"
-                :response-format (ajax/json-response-format {:keywords? true})
-                :on-success      [::good-http-result "test-token1"]
-                :on-failure      [::bad-http-result "test-token1"]}}))
-    ;; setup success handler
-    (re-frame/def-event
-      ::good-http-result
-      (fn [db [_ token result]]
-        (is (= "test-token1" token) "expected: token to passed through")
-        (is (map? result) "expected: result should be a map")
-        (is (every? keyword? (keys result)) "all keys should be keywords as per :response-format")
-        ;; check shape of result using loose Spec
-        (is (not= (s/conform ::api-result result) :cljs.spec/invalid)
-            (s/explain-str ::api-result result))
-        db))
-    ;; kick things off
-    (re-frame/dispatch [::http-test])
-    ;; give re-frame and http time to process
+(deftest ajax-get
+  ;; Setup handler which returns a :http effect specifying an ajax-request.
+  ;; Note we specify optional :response-format to make sure our json result
+  ;; has keywords for keys, and :timeout see the ajax-request API
+  ;; for more details https://github.com/JulianBirch/cljs-ajax#ajax-request
+  ;; We specify an :on-failure for completenes but we don't expect the request
+  ;; to fail unless there is something wrong with your internet or github.
+  (re-frame/def-event-fx
+    ::http-test
+    (fn [_context _event-v]
+      {:http {:method          :get
+              :uri             "https://api.github.com/orgs/day8"
+              :timeout         5000
+              :response-format (ajax/json-response-format {:keywords? true})
+              :on-success      [::good-http-result "test-token1"]
+              :on-failure      [::bad-http-result "test-token1"]}}))
     (async done
-       (js/setTimeout
-         (fn []
-           (is (@seen-events ::good-http-result) "::good-http-result should have been fired")
-           (done)
-           (re-frame/remove-post-event-callback forwarder))
-         3000))))
+
+      ;; setup success handler
+      (re-frame/def-event
+        ::good-http-result
+        (fn [db [_ token result]]
+          (is (= "test-token1" token) "expected: token passed through")
+          ;; check shape of result using loose Spec
+          (is (not= (s/conform ::api-result result) :cljs.spec/invalid)
+              (s/explain-str ::api-result result))
+          (is (every? keyword? (keys result)) "keys should be keywords")
+          (done)
+          db))
+
+      ;; setup failure handler
+      (re-frame/def-event
+        ::bad-http-result
+        (fn [db [_ _token error]]
+          (re-frame/console :error "Unexpected HTTP error, something wrong with your internet? " error)
+          (done)
+          db))
+
+      ;; kick things off
+      (re-frame/dispatch [::http-test])))
