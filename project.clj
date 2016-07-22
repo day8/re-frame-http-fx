1ff58685fe893abc3e00ea3181ee65a2f49e44e5
Mike Thompson <mike.thompson@day8.com.au>
2016-07-22 22:53:52 +1000

    v0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/http-fx "0.0.2-SNAPSHOT"
+(defproject day8.re-frame/http-fx "0.0.2"
   :description  "A re-frame effects handler for performing Ajax tasks"
   :url          "https://github.com/Day8/re-frame-http-fx.git"
   :license      {:name "MIT"}
