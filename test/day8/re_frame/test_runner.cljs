5328dc144b6f9ceac2a40f78f3077f5f1549e8c8
hipitihop <anonymous.amphibian@gmail.com>
2016-08-24 08:47:55 +1000

    Bump binaryage/devtools "0.8.1" & use :preloads

--- test/day8/re_frame/test_runner.cljs
+++ test/day8/re_frame/test_runner.cljs
@@ -2,13 +2,11 @@
   (:require
     [cljs.test         :as cljs-test :include-macros true]
     [jx.reporter.karma :as karma :include-macros true]
-    [devtools.core     :as devtools]
     ;; Test Namespaces -------------------------------
     [day8.re-frame.http-fx-test])
   (:refer-clojure :exclude (set-print-fn!)))
 
 (enable-console-print!)
-(devtools/install! [:custom-formatters :sanity-hints]) ;; we love https://github.com/binaryage/cljs-devtools
 
 ;; ---- BROWSER based tests ----------------------------------------------------
 (defn ^:export set-print-fn! [f]
