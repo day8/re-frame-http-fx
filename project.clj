ba0c21df7aac2507ef429e075b3188e2c029a62d
Daniel Compton <desk@danielcompton.net>
2016-10-10 15:39:38 +1300

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject day8.re-frame/http-fx "0.1.0-SNAPSHOT"
+(defproject day8.re-frame/http-fx "0.1.0"
   :description  "A re-frame effects handler for performing Ajax tasks"
   :url          "https://github.com/Day8/re-frame-http-fx.git"
   :license      {:name "MIT"}
